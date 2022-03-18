166cb925d8d72ba9c36cf2442f205805980b9d41
cttynul <cttynul@outlook.com>
2022-03-18 13:57:24 +0100

    yay

--- dosbox-pi.sh
+++ dosbox-pi.sh
@@ -1,8 +1,8 @@
 #!\bin\bash
 
 reboot_target () {
-  read -p "Press any key to reboot" par
-  reboot now
+  read -p "Press ENTER to reboot " par
+  sudo reboot now
 }
 
 echo "    __             __                             __ "
@@ -10,15 +10,10 @@ echo ".--|  .-----.-----|  |--.-----.--.--.______.-----|__|"
 echo "|  _  |  _  |__ --|  _  |  _  |_   _|______|  _  |  |"
 echo "|_____|_____|_____|_____|_____|__.__|      |   __|__|"
 echo " -cttynul                                  |__|      "
+echo ""
+read -p "Are you currently running this on a Raspberry PI? [Y|n] " rpi_op
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-read -p "Are you currently running this on a Raspberry PI? [Y|n]" rpi_op
-
-if [ "$rpi_op" -eq "Y" ]
+if [ "$rpi_op" == "Y" ]
   then 
     echo To autologin perform following steps
     echo "> raspi-config"
@@ -28,15 +23,15 @@ if [ "$rpi_op" -eq "Y" ]
     echo Select Finish, and reboot the Raspberry Pi.
 fi
 
-read -p "Do you want to turn your device into a Dosbox Machine? [Y|n]" option
+read -p "Do you want to turn your device into a Dosbox Machine? [Y|n] " option
 
-if [ "$option" -ne "Y" ]
+if [ "$option" != "Y" ]
   then echo "Exiting..."
   exit
 else
-  apt update
+  sudo apt update
   # Installing DOSBox
-  apt -y install dosbox
+  sudo apt -y install dosbox
   mkdir -p ~/DOS/C
   mkdir -p ~/DOS/A
   
@@ -54,18 +49,18 @@ else
   echo DIR C:\ >> `ls ~/.dosbox/dosbox-*.conf`
   
   # Setting up samba share for C and A DOSbox Drives
-  apt install -y samba samba-common-bin
-  chmod 777 ~/DOS/C
-  chmod 777 ~/DOS/A
-  echo [DOSBox] >> /etc/samba/smb.conf
-  echo path=~/DOS/ >> /etc/samba/smb.conf
-  echo writeable=yes  >> /etc/samba/smb.conf
-  echo create mask=0777 >> /etc/samba/smb.conf
-  echo directory mask=0777 >> /etc/samba/smb.conf
-  echo public=yes >> /etc/samba/smb.conf
-  echo read only=no >> /etc/samba/smb.conf
-  systemctl enable smbd
-  systemctl restart smbd
+  sudo apt install -y samba samba-common-bin
+  sudo chmod 777 ~/DOS/C
+  sudo chmod 777 ~/DOS/A
+  sudo echo [DOSBox] >> /etc/samba/smb.conf
+  sudo echo path=~/DOS/ >> /etc/samba/smb.conf
+  sudo echo writeable=yes  >> /etc/samba/smb.conf
+  sudo echo create mask=0777 >> /etc/samba/smb.conf
+  sudo echo directory mask=0777 >> /etc/samba/smb.conf
+  sudo echo public=yes >> /etc/samba/smb.conf
+  sudo echo read only=no >> /etc/samba/smb.conf
+  sudo systemctl enable smbd
+  sudo systemctl restart smbd
   echo "You can access your DOS drive from another PC in your LAN browsing:"
   echo smb://$(hostname) or smb://$(hostname -I)
   echo "Enjoy"
@@ -73,31 +68,31 @@ else
 fi
 
 # RPI Settings
-if [ "$rpi_op" -eq "Y" ]
+if [ "$rpi_op" == "Y" ]
   then 
     #forcing 640x480 pi resolution
-    echo hdmi_group=1 >> /boot/config.txt
-    echo hdmi_mode=1 >> /boot/config.txt
+    sudo echo hdmi_group=1 >> /boot/config.txt
+    sudo echo hdmi_mode=1 >> /boot/config.txt
 fi
 
-read -p "Do you want to install RetroPie? [Y|n]" option
-if [ "$option" -ne "Y" ]
+read -p "Do you want to install RetroPie? [Y|n] " option
+if [ "$option" != "Y" ]
   then reboot_target
 else
-  apt install -y git
-  apt install -y libsdl*dev
+  sudo apt install -y git
+  sudo apt install -y libsdl*dev
   git clone https://github.com/RetroPie/RetroPie-Setup.git
   chmod +x ./RetroPie-Setup/retropie_setup.sh
   ./RetroPie-Setup/retropie_setup.sh
   chmod 777 ~/RetroPie
-  echo [RetroPie] >> /etc/samba/smb.conf
-  echo path=~/RetroPie >> /etc/samba/smb.conf
-  echo writeable=yes  >> /etc/samba/smb.conf
-  echo create mask=0777 >> /etc/samba/smb.conf
-  echo directory mask=0777 >> /etc/samba/smb.conf
-  echo public=yes >> /etc/samba/smb.conf
-  echo read only=no >> /etc/samba/smb.conf
-  systemctl restart smbd
+  sudo echo [RetroPie] >> /etc/samba/smb.conf
+  sudo echo path=~/RetroPie >> /etc/samba/smb.conf
+  sudo echo writeable=yes  >> /etc/samba/smb.conf
+  sudo echo create mask=0777 >> /etc/samba/smb.conf
+  sudo echo directory mask=0777 >> /etc/samba/smb.conf
+  sudo echo public=yes >> /etc/samba/smb.conf
+  sudo echo read only=no >> /etc/samba/smb.conf
+  sudo systemctl restart smbd
   echo "You can access your RetroPie folder from another PC in your LAN browsing:"
   echo smb://$(hostname) or smb://$(hostname -I)
   echo "To run Retropie you need to type emulationstation in your Rasbian CLI"
